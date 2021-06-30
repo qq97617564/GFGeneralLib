1c3c7e99efa69696efdb2a05a1b0cdff3a821c09
王刚锋 <597617564@qq.com>
2021-06-30 16:53:59 +0800

    fix

---
+++ GFGeneralLib/GFGeneralLib.h
@@ -0,0 +1,13 @@
+//
+//  GFGeneraManager.h
+//  Pods
+//
+//  Created by 王刚锋 on 2021/6/28.
+//
+
+#ifndef GFGeneraManager_h
+#define GFGeneraManager_h
+
+#import "BaseClassHeader.h"
+
+#endif /* GFGeneraManager_h */
